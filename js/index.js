a1d08c69fb60f835e39470ca319fc5e11306128e
ktspriggs <katiespriggs@Katies-MBP.fios-router.home>
2020-04-12 11:43:51 -0400

    Animated the hamburger menu

--- js/index.js
+++ js/index.js
@@ -3,21 +3,30 @@ $(document).ready(function() {
     var burgerClosed = $('.burger-closed');
     var burgerOpen = $('.burger-open');
     var mobileMenu = $('.mobile-menu');
+    var mobileMenuLink = $('.menu li a')
 
     mobileMenuBurger.on('click', function () {
         burgerOpen.toggleClass('display-none');
         burgerClosed.toggleClass ('display-none');
         mobileMenu.toggleClass('display-none');
     });
+
+    burgerClosed.on('click', function () {
+       mobileMenu.slideToggle("slow");
+    });
+
+    burgerOpen.on('click', function () {
+        mobileMenu.slideToggle("slow");
+
+     });
+
+     mobileMenuLink.on('click', function () {
+        mobileMenu.slideToggle("slow");
+        burgerOpen.toggleClass('display-none');
+        burgerClosed.toggleClass('display-none');
+     });
+
 });
-
-
-
-
-
-
-
-
 
 
 
