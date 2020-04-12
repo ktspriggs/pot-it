0e288ee325ad4949ca1c5c56fef76343bfdcf6d6
ktspriggs <katiespriggs@Katies-MBP.fios-router.home>
2020-04-12 14:26:54 -0400

    Fixing leaves and opacity of mobile menu

--- js/index.js
+++ js/index.js
@@ -3,27 +3,33 @@ $(document).ready(function() {
     var burgerClosed = $('.burger-closed');
     var burgerOpen = $('.burger-open');
     var mobileMenu = $('.mobile-menu');
-    var mobileMenuLink = $('.menu li a')
+    var mainContent = $('.main-content');
+    var mobileMenuLink = $('.menu li a');
 
+    //menu opens and closes and main content opacity changes//
     mobileMenuBurger.on('click', function () {
         burgerOpen.toggleClass('display-none');
         burgerClosed.toggleClass ('display-none');
         mobileMenu.toggleClass('display-none');
+        mainContent.toggleClass('opacity');
     });
 
+    //click on burgerClosed menu transitions open//
     burgerClosed.on('click', function () {
        mobileMenu.slideToggle("slow");
     });
 
+    //click on burgerOpen menu transitions closed//
     burgerOpen.on('click', function () {
         mobileMenu.slideToggle("slow");
-
      });
 
+    //when nav-link clicked menu changes to burgerClosed and opacity changes//
      mobileMenuLink.on('click', function () {
-        mobileMenu.slideToggle("slow");
         burgerOpen.toggleClass('display-none');
         burgerClosed.toggleClass('display-none');
+        mobileMenu.slideToggle("slow");
+        mainContent.toggleClass('opacity');
      });
 
 });
